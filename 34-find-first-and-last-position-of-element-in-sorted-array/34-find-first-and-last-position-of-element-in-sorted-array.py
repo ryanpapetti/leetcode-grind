63f78eccc83be86df5a8901e3d6494b21540a2ae
Ryan Papetti <ryanpapetti@gmail.com>
2022-03-11 16:10:13 -0700

    Time: 108 ms (61.51%), Space: 46.5 MB (12.72%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,12 +2,12 @@ class Solution:
     
     
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        #the O(logn) hint suggests I use Binary Search, which made sense regardless
+        #the O(logn) hint suggests I use a left/right pointer approach, which made sense regardless
 
         left_pointer = 0
         right_pointer = len(nums)-1
         
-        def binary_search(left_pointer, right_pointer ):
+        def custom_search(left_pointer, right_pointer ):
         
             if not nums:
                 return [-1,-1]
@@ -29,11 +29,11 @@ class Solution:
                 right_pointer -=1
 
             if right_val == target:
-                return binary_search(left_pointer, right_pointer)
+                return custom_search(left_pointer, right_pointer)
             if left_val == target:
-                return binary_search(left_pointer, right_pointer)
+                return custom_search(left_pointer, right_pointer)
 
-            return binary_search(left_pointer, right_pointer)
+            return custom_search(left_pointer, right_pointer)
         
-        return binary_search(left_pointer, right_pointer)
+        return custom_search(left_pointer, right_pointer)
             
