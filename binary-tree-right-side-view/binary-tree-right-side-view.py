d5a0e922f994f72d22add693c16e639a840940fa
Ryan Papetti <ryanpapetti@gmail.com>
2022-03-11 17:10:12 -0700

    Time: 356 ms, Memory: 13.8 MB - LeetHub

--- binary-tree-right-side-view/binary-tree-right-side-view.py
+++ binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,26 +6,26 @@
 #         self.right = right
 class Solution:
     
-    def has_child(self,node):
-        return node.right or node.left
+#     def has_child(self,node):
+#         return node.right or node.left
     
-    def special_traverse(self,root, ignore_root = False):
-        if root is None:
-            return []
-        proper_root_val = [root.val] if not ignore_root else []
+#     def special_traverse(self,root, ignore_root = False):
+#         if root is None:
+#             return []
+#         proper_root_val = [root.val] if not ignore_root else []
 
-        if root.right is None and root.left:
-            return proper_root_val + self.special_traverse(root.left)
-        if root.right and not self.has_child(root.right) and root.left and self.has_child(root.left):
-            return proper_root_val + [root.right.val] + self.special_traverse(root.left, ignore_root = True)
-        return proper_root_val + self.special_traverse(root.right) + self.special_traverse(root.left)[len(self.special_traverse(root.right)):]
+#         if root.right is None and root.left:
+#             return proper_root_val + self.special_traverse(root.left)
+#         if root.right and not self.has_child(root.right) and root.left and self.has_child(root.left):
+#             return proper_root_val + [root.right.val] + self.special_traverse(root.left, ignore_root = True)
+#         return proper_root_val + self.special_traverse(root.right) + self.special_traverse(root.left)[len(self.special_traverse(root.right)):]
             
     
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         
         if root is None:
             return []
-        partial_correct_view = [root.val] + self.special_traverse(root.right) 
-        return partial_correct_view + self.special_traverse(root.left)[len(self.special_traverse(root.right)):]
+        partial_correct_view = [root.val] + self.rightSideView(root.right) 
+        return partial_correct_view + self.rightSideView(root.left)[len(self.rightSideView(root.right)):]
         
         
