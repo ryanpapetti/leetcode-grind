530eb080d7a3c77b456348424eec17d782c3695a
Ryan Papetti <ryanpapetti@gmail.com>
2022-03-13 12:46:33 -0700

    Time: 24 ms (99.94%), Space: 13.9 MB (87.03%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -4,41 +4,14 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        
-        def add_at_end(node,new_node):
-            if node is None:
-                node = new_node
-                return None
-            last = node
-            while last.next:
-                last = last.next
-            
-            last.next = new_node
-        
-        list3 = ListNode()
-        
-        while list1 is not None or list2 is not None:
-            if list1 and list2:
-                if list1.val >= list2.val:
-                    #add both - one way to do this is just to add one at random and the other one will get caught next turn
-                    # i like 2 hehe
-                    new_node = ListNode(list2.val)
-                    add_at_end(list3,new_node)
-                    list2 = list2.next
-
-                else:
-                    new_node = ListNode(list1.val)
-                    add_at_end(list3,new_node)
-                    list1 = list1.next
-            elif list1:
-                new_node = ListNode(list1.val)
-                add_at_end(list3,new_node)
-                list1 = list1.next
-            elif list2:
-                new_node = ListNode(list2.val)
-                add_at_end(list3,new_node)
-                list2 = list2.next
-                
-        return list3.next
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
