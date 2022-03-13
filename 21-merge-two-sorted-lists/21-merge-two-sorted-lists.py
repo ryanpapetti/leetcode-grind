3651f10a1a54c8f477b148cedb93140e6895f0b2
Ryan Papetti <ryanpapetti@gmail.com>
2022-03-13 12:47:59 -0700

    Time: 60 ms (36.08%), Space: 13.8 MB (87.03%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,13 +5,13 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1, l2):
-        if l1 is None:
+        if l1 is None: #base cases
             return l2
-        elif l2 is None:
+        elif l2 is None: #base cases
             return l1
-        elif l1.val < l2.val:
+        elif l1.val < l2.val: #if l2 is bigger than l1, the l1 good - move down it
             l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1
+            return l1 #once all the recursion stacks are hit, this completely sorts it... recursion is nice
         else:
             l2.next = self.mergeTwoLists(l1, l2.next)
             return l2
