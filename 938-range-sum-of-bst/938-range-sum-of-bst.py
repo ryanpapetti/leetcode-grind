359a8515f282c58774033e145fb7146de510e906
Ryan Papetti <ryanpapetti@gmail.com>
2022-03-15 10:54:59 -0700

    Time: 436 ms (13.14%), Space: 23.1 MB (39.41%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -6,16 +6,16 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        total = [0]
+        self.total = 0
         def traverse_tree(root):
             if root is None:
                 return None
             
             if root.val >= low and root.val <= high:
-                total[0] += root.val
+                self.total += root.val
             
             traverse_tree(root.left)
             traverse_tree(root.right)
         
         traverse_tree(root)
-        return total[0]
+        return self.total
