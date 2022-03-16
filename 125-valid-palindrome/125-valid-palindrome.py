6a885d67466ee9c9ea84e7ef0fe43c94ce9895c0
Ryan Papetti <ryanpapetti@gmail.com>
2022-03-15 17:44:44 -0700

    Time: 67 ms (53.24%), Space: 14.4 MB (91.02%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,6 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        final_str = (''.join([char for char in s if char.isalpha() or char.isnumeric()])).lower()
+#         final_str = (''.join([char for char in s if char.isalpha() or char.isnumeric()])).lower()
         
-        return final_str == final_str[::-1]
+#         return final_str == final_str[::-1]
         
+        
+        left_pointer, right_pointer = 0, len(s)-1
+        while right_pointer > left_pointer:
+            #now we need to check if the character is valid. if it aint, keep moving
+            while right_pointer > left_pointer and not s[left_pointer].isalnum():
+                left_pointer +=1
+                
+            while right_pointer > left_pointer and not s[right_pointer].isalnum():
+                right_pointer -=1
+            
+            left_val, right_val = s[left_pointer].lower(), s[right_pointer].lower()
+            
+            if left_val != right_val:
+                return False
+            left_pointer +=1
+            right_pointer -=1
+        return True
