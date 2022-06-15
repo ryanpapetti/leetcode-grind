bc1593db6504ee8d0f059d1b7533fa4e4e4049db
Ryan Papetti <ryanpapetti@gmail.com>
2022-06-15 00:31:48 -0700

    Time: 386 ms (53.98%), Space: 0B (100.00%) - LeetHub

--- 1369-get-the-second-most-recent-activity/1369-get-the-second-most-recent-activity.sql
+++ 1369-get-the-second-most-recent-activity/1369-get-the-second-most-recent-activity.sql
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
-WITH activity_ranks AS (SELECT username, activity, startDate, endDate, RANK() OVER (PARTITION BY username ORDER BY endDate DESC) as rank_activity FROM UserActivity),
-
-activity_counts AS (SELECT username, activity, startDate, endDate, COUNT(*) AS activity_count FROM UserActivity GROUP BY username HAVING COUNT(*)=1)
+WITH activity_ranks AS (SELECT username, activity, startDate, endDate, RANK() OVER (PARTITION BY username ORDER BY endDate DESC) AS rank_activity, COUNT(*) OVER (PARTITION BY username) AS count_activity  FROM UserActivity)
 
 
-SELECT username, activity, startDate, endDate FROM activity_ranks WHERE rank_activity=2
-UNION
-SELECT username, activity, startDate, endDate FROM activity_counts
+SELECT username, activity, startDate, endDate FROM activity_ranks WHERE rank_activity=2 OR count_activity=1
