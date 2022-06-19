831f5c24817e483c675147feae7727ac4e52bb42
Ryan Papetti <ryanpapetti@gmail.com>
2022-06-19 14:20:27 -0700

    Time: 1717 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1194-tournament-winners/1194-tournament-winners.sql
+++ 1194-tournament-winners/1194-tournament-winners.sql
@@ -5,9 +5,6 @@
 # we need to then rank these players partitioning by each group (i.e get the group rank)
 # return all players whose rank is one
 
-
-# getting total player points - if they are player 1, then do first_score if they are player two then do second_score otherwise 0
-
 WITH first_player_scores AS (SELECT player_id, IFNULL(SUM(first_score),0) AS first_scores FROM Matches RIGHT JOIN Players ON first_player=player_id GROUP BY player_id ),
 
 second_player_scores AS (SELECT player_id, IFNULL(SUM(second_score),0) AS second_scores FROM Matches RIGHT JOIN Players ON second_player=player_id  GROUP BY player_id),
@@ -18,6 +15,3 @@ total_player_scores AS (SELECT first_player_scores.player_id, first_scores+secon
 players_group_rank AS (SELECT Players.player_id, group_id, RANK() OVER (PARTITION BY group_id ORDER BY total_score DESC, total_player_scores.player_id) AS group_rank, total_score FROM total_player_scores JOIN Players ON  Players.player_id = total_player_scores.player_id)
 
 SELECT group_id, player_id FROM players_group_rank WHERE group_rank=1
-
-# SELECT * FROM combined_player_scores
-
